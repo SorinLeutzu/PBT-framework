711a10300419a5235924b80dd0462d9fbeffddc2
SorinLeutzu <sorin1harjan1@gmail.com>
2025-08-04 09:04:50 +0300

    assertions v2

--- Assertions.hs
+++ Assertions.hs
@@ -1,85 +1,143 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
+
 module Assertions
-(Matcher(..),EqMatcher(..),GtMatcher(..),ContainsMatcher(..),
-And(..),Or(..),Not(..),
-eq,gt,contains,
-assertThat, expectThat) where
+  ( Matcher (..),
+    EqMatcher (..),
+    GtMatcher (..),
+    ContainsMatcher (..),
+    And (..),
+    Or (..),
+    Not (..),
+    eq,
+    gt,
+    contains,
+    assertThat,
+    expectThat,
+  )
+where
 
-
-
---Checks if a string contains a particular string
+-- Checks if a string contains a particular string
 containsString _ [] = False
-containsString string text@(t:ts) = if (startsWithString string text) then True else (containsString string ts)
+containsString string text@(t : ts) = if (startsWithString string text) then True else (containsString string ts)
 
---Checks if a string starts with a particular string
+-- Checks if a string starts with a particular string
 startsWithString [] _ = True
 startsWithString _ [] = False
-startsWithString string@(s:ss) text@(t:ts) | t==s = startsWithString ss ts
-                                           | t/=s = False
+startsWithString string@(s : ss) text@(t : ts)
+  | t == s = startsWithString ss ts
+  | t /= s = False
 
 -- Mathcer typeclass
-class Matcher m a where
-    match:: m->a-> Either String()
+class Matcher m a | m -> a where
+  matches :: m -> a -> Bool
+  describe :: m -> Bool -> String
 
---Basic matchers:
+  explainMatch :: m -> a -> String
+  explainMatch m x =
+    let ok = matches m x
+        desc = describe m ok
+     in if ok
+          then "which " ++ desc
+          else "which does not " ++ desc
 
---Equality matcher
+-- Basic matchers:
+
+-- Equality matcher
 newtype EqMatcher a = EqMatcher a
-instance (Eq a,Show a) => Matcher (EqMatcher a) a where
-    match (EqMatcher exp) act 
-     | exp == act = Right()
-     | otherwise = Left $ "Expected  "++ show exp++ "but got "++show act
 
---Comparison Mathcer
+instance (Eq a, Show a) => Matcher (EqMatcher a) a where
+  matches (EqMatcher exp) act = exp == act
+  describe :: (Eq a, Show a) => EqMatcher a -> Bool -> String
+  describe (EqMatcher act) ok
+    | ok = "is equal to " ++ show act
+    | otherwise = "is not equal to " ++ show act
+
+-- Comparison Mathcer
 newtype GtMatcher a = GtMatcher a
-instance (Ord a,Show a) => Matcher (GtMatcher a) a where
-    match (GtMatcher higher) lower
-     |higher>lower = Right()
-     |otherwise = Left $ show higher ++ "is not greater than"++show lower
 
+instance (Ord a, Show a) => Matcher (GtMatcher a) a where
+  matches (GtMatcher higher) lower = higher > lower
+  describe (GtMatcher higher) ok
+    | ok = "is greater than " ++ show higher
+    | otherwise = "id not greater than " ++ show higher
 
---Contains matcher
+-- Contains matcher
 newtype ContainsMatcher = ContainsMatcher String
+
 instance Matcher ContainsMatcher String where
-    match (ContainsMatcher string) text
-     | containsString string text= Right()
-     | otherwise = Left $ "String "++ show text ++" does not contain string " ++show string
+  matches (ContainsMatcher string) text = containsString string text
+  describe (ContainsMatcher string) ok
+    | ok = "Contains substring " ++ show string
+    | otherwise = "does not contain " ++ show string
+  explainMatch (ContainsMatcher string) text
+    | matches (ContainsMatcher string) text = "which contains \"" ++ string ++ "\" in \"" ++ text ++ "\""
+    | otherwise = "which does not contain \"" ++ string ++ "\" in \"" ++ text ++ "\""
 
 eq = EqMatcher
+
 gt = GtMatcher
+
 contains = ContainsMatcher
 
+-- Matcher combinators
 
+-- And combinator
 data And m1 m2 a where
-    And::(Matcher m1 a,Matcher m2 a) => m1->m2->And m1 m2 a
-instance (Matcher m1 a,Matcher m2 a) => Matcher (And m1 m2 a) a where
-    match (And m1 m2) x = do
-        match m1 x
-        match m2 x
+  And :: (Matcher m1 a, Matcher m2 a) => m1 -> m2 -> And m1 m2 a
 
+instance (Matcher m1 a, Matcher m2 a) => Matcher (And m1 m2 a) a where
+  matches (And m1 m2) x = matches m1 x && matches m2 x
+  describe (And m1 m2) ok =
+    let d1 = describe m1 ok
+        d2 = describe m2 ok
+        conJ = if ok then "and" else "nor"
+     in "has both properties (" ++ d1 ++ ")" ++ conJ ++ " (" ++ d2 ++ ")"
+  explainMatch (And m1 m2) x = case (firstMatch, secondMatch) of
+    (False, _) -> "which fails first part " ++ explainMatch m1 x
+    (_, False) -> "which fails second part " ++ explainMatch m2 x
+    _ -> "which satisfies both"
+    where
+      firstMatch = matches m1 x
+      secondMatch = matches m2 x
+
+-- Or Combinator
 data Or m1 m2 a where
-    Or::(Matcher m1 a, Matcher m2 a) => m1->m2-> Or m1 m2 a
-instance (Matcher m1 a,Matcher m2 a) => Matcher (Or m1 m2 a) a where
-    match (Or m1 m2) x=
-        case match m1 x of
-            Right() -> Right()
-            Left e1 -> case match m2 x of
-                Right() -> Right()
-                Left e2 -> Left(e1++" or  "++e2)
+  Or :: (Matcher m1 a, Matcher m2 a) => m1 -> m2 -> Or m1 m2 a
+
+instance (Matcher m1 a, Matcher m2 a) => Matcher (Or m1 m2 a) a where
+  matches (Or m1 m2) x = matches m1 x || matches m2 x
+  describe (Or m1 m2) ok =
+    let d1 = describe m1 ok
+        d2 = describe m2 ok
+     in if ok
+          then "matches at least one: (" ++ d1 ++ ") or (" ++ d2 ++ ")"
+          else "matches none (" ++ d1 ++ ") nor (" ++ d2 ++ ")"
+  explainMatch (Or m1 m2) x
+    | matches m1 x = "which satisfies first: " ++ explainMatch m1 x
+    | matches m2 x = "which satisfies second" ++ explainMatch m2 x
+    | otherwise = "which satisfies neither: first " ++ describe m1 False ++ "; second " ++ describe m2 False
 
 data Not m a where
-    Not :: Matcher m a=> m-> Not m a
-instance Matcher m a => Matcher (Not m a) a where
-    match (Not m) x = case match m x of
-        Right() -> Left $ "Negation failed"
-        Left _ -> Right()
+  Not :: (Matcher m a) => m -> Not m a
 
-               
+instance (Matcher m a) => Matcher (Not m a) a where
+  matches (Not m) x = not (matches m x)
+  describe (Not m) ok
+    | ok = "does not" ++ d
+    | otherwise = "unexpectedly" ++ d
+    where
+      d = describe m (not ok)
+  explainMatch (Not m) x
+    | matches m x = "which unexpectedly matches: " ++ describe m True
+    | otherwise = "which correctly does not match: " ++ describe m False
 
+assertThat :: (Matcher m a, Show a) => m -> a -> IO ()
+assertThat m x
+  | matches m x = return ()
+  | otherwise = error $ "Assertion failed: value " ++ show x ++ " " ++ explainMatch m x
 
-assertThat :: Matcher m a=> m->a->IO()
-assertThat m x = case match m x of
-    Right() -> return()
-    Left err ->  error("Assertion failed " ++ err)
-
-expectThat :: Matcher m a => m->a->IO (Either String())
-expectThat m x = return(match m x)
+expectThat :: (Matcher m a) => m -> a -> IO (Either String ())
+expectThat m x
+  | matches m x = return (Right ())
+  | otherwise = return (Left (explainMatch m x))
--- Test.hs
+++ Test.hs
@@ -2,24 +2,23 @@ module Main where
 
 import Assertions
 
+report :: String -> Either String () -> IO ()
+report label (Right ()) = putStrLn $ "Non-fatal tests passed: " ++ label
+report label (Left err) = putStrLn $ "Non-fatal tests failed" ++ label ++ " : " ++ err
 
-report:: String-> Either String () -> IO()
-report label (Right()) = putStrLn $ "Non-fatal tests passed: "++label
-report label (Left err) = putStrLn $ "Non-fatal tests failed"++ label++" : "++err
-
-main::IO()
+main :: IO ()
 main = do
-    putStrLn "Running assertions: "
-    assertThat (eq (5::Int)) (5::Int)
-    putStrLn "Passed first assertion"
+  putStrLn "Running assertions: "
+  assertThat (eq (5 :: Int)) (5 :: Int)
+  putStrLn "Passed first assertion"
 
-    expectThat( contains "xyz") ("abcxyd") >>= report " contains string: "
-    
-    let combo :: And (GtMatcher Int) (Not (EqMatcher Int) Int) Int
-        combo = gt (8::Int) `And` Not (eq (5::Int))
+  expectThat (contains "xyz") ("abcxyd") >>= report " contains string: "
+  expectThat (contains "xyz") ("bcxyz") >>= report " contains string: "
 
-    expectThat combo (5::Int) >>= report "combo 5" -- this should not work
-    expectThat combo (6::Int) >>= report "combo 6" -- this should work
+  let combo :: And (GtMatcher Int) (Not (EqMatcher Int) Int) Int
+      combo = gt (8 :: Int) `And` Not (eq (5 :: Int))
 
-    assertThat (gt (10::Int)) (50::Int)
-    putStrLn "Passed second assertion" -- this should not be printed
+  expectThat combo (5 :: Int) >>= report "combo 5" -- this should not work
+  expectThat combo (6 :: Int) >>= report "combo 6" -- this should work
+  assertThat (gt (10 :: Int)) (50 :: Int)
+  putStrLn "Passed second assertion" -- this should not be printed
