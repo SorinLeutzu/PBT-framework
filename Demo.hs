10bb590f17c618bb77652289bb1447d0fbe7e734
SorinLeutzu <sorin1harjan1@gmail.com>
2025-11-13 11:16:08 +0200

    updated Demo

--- Demo.hs
+++ Demo.hs
@@ -1,13 +1,31 @@
-module Demo where
+module Demo
+  ( LogEntry (..),
+    applyShrinkRaw,
+    assertThenShrinkRaw,
+    renderLog,
+    renderLogs,
+    formatResult,
+    applyShrink,
+    assertThenShrink,
+  )
+where
 
-import Assertions (Matcher (..), eq)
+import Control.Monad (replicateM, when)
 import Control.Monad.Writer (Writer, runWriter, tell)
-import Random (shrinkIntAgg, shrinkIntStd)
+import Data.List (take)
+import Matchers.Core (Matcher (..), eq, gt)
+import Random qualified as Rand
 
-applyShrink :: (Matcher m a, Show a) => a -> m -> (a -> [a]) -> Writer [String] a
-applyShrink element matcher getShrinkingCandidates = do
+data LogEntry a = LogEntry
+  { leInput :: a,
+    lePassed :: Bool
+  }
+  deriving (Eq, Show)
+
+applyShrinkRaw :: (Matcher m a) => a -> m -> (a -> [a]) -> Writer [LogEntry a] a
+applyShrinkRaw element matcher getShrinkingCandidates = do
   let initialPassed = matches matcher element
-  tell ["Tried input " ++ show element ++ " - Assertion " ++ (if initialPassed then "passed" else "failed")]
+  tell [LogEntry element initialPassed]
   if initialPassed
     then return element
     else go element (getShrinkingCandidates element)
@@ -15,27 +33,44 @@ applyShrink element matcher getShrinkingCandidates = do
     go lastFail [] = return lastFail
     go lastFail (c : cs) = do
       let passed = matches matcher c
-      tell ["Tried input " ++ show c ++ " - Assertion " ++ (if passed then "passed" else "failed")]
+      tell [LogEntry c passed]
       if passed
         then return lastFail
         else go c cs
 
-assertThenShrink :: (Matcher m a, Show a) => a -> m -> (a -> [a]) -> Writer [String] String
-assertThenShrink element matcher getShrinkingCandidates
+assertThenShrinkRaw :: (Matcher m a) => a -> m -> (a -> [a]) -> Writer [LogEntry a] (Maybe a)
+assertThenShrinkRaw element matcher getShrinkingCandidates
   | matches matcher element = do
-      tell ["Tried input " ++ show element ++ " - Assertion passed"]
-      return "Matching succeeded"
+      tell [LogEntry element True]
+      return Nothing
   | otherwise = do
-      minimal <- applyShrink element matcher getShrinkingCandidates
-      return $ "Matching failled. Searching for simplest counter example " ++ show minimal
+      minimal <- applyShrinkRaw element matcher getShrinkingCandidates
+      return (Just minimal)
+
+renderLog :: (Show a) => LogEntry a -> String
+renderLog (LogEntry x passed) =
+  "Tried input " ++ show x ++ " - Assertion " ++ (if passed then "passed" else "failed")
+
+renderLogs :: (Show a) => [LogEntry a] -> [String]
+renderLogs = map renderLog
+
+formatResult :: (Show a) => Maybe a -> String
+formatResult Nothing = "Matching succeeded"
+formatResult (Just minimal) = "Matching failled. Searching for simplest counter example " ++ show minimal
+
+applyShrink :: (Matcher m a, Show a) => a -> m -> (a -> [a]) -> (a, [String])
+applyShrink el matcher shrinker =
+  let (res, logs) = runWriter (applyShrinkRaw el matcher shrinker)
+   in (res, renderLogs logs)
+
+assertThenShrink :: (Matcher m a, Show a) => a -> m -> (a -> [a]) -> (String, [String])
+assertThenShrink el matcher shrinker =
+  let (maybeMin, logs) = runWriter (assertThenShrinkRaw el matcher shrinker)
+      msg = formatResult maybeMin
+   in (msg, renderLogs logs)
 
 t1 :: (Int, [String])
-t1 = runWriter (applyShrink (64 :: Int) (eq (8 :: Int)) shrinkIntStd)
+t1 = applyShrink (64 :: Int) (eq (8 :: Int)) Rand.shrinkIntStd
 
--- ghci> t1
--- (16,["Tried input 64 - Assertion failed","Tried input 64 - Assertion failed","Tried input 32 - Assertion failed","Tried input 16 - Assertion failed","Tried input 8 - Assertion passed"])
 t2 :: (String, [String])
-t2 = runWriter (assertThenShrink (40940 :: Int) (eq (144 :: Int)) shrinkIntAgg)
-
--- ghci> t2
--- ("Matching failled. Searching for simplest counter example 1",["Tried input 40940 - Assertion failed","Tried input 40940 - Assertion failed","Tried input 16 - Assertion failed","Tried input 4 - Assertion failed","Tried input 2 - Assertion failed","Tried input 1 - Assertion failed"])
+t2 = assertThenShrink (40940 :: Int) (eq (144 :: Int)) Rand.shrinkIntAgg
