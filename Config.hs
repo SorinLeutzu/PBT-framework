c526da5e7250a57d2cb96e9565ca85fee1324d84
SorinLeutzu <sorin1harjan1@gmail.com>
2026-06-25 18:18:10 +0300

    shrinking options

--- Config.hs
+++ Config.hs
@@ -1,5 +1,13 @@
 module Config where
 
+data ShrinkingImpl = Sequential | DeterministicParallel | NonDeterministicParallel deriving (Show, Eq)
+
+shrinkingImplementation :: ShrinkingImpl
+shrinkingImplementation = DeterministicParallel
+
+chunkSize :: Int
+chunkSize = 20
+
 withColors :: Bool
 withColors = True
 
